5c6305a98e2058f378f1167981f56ec601e3d66c
Shabin k <73272797+SHABIN-K@users.noreply.github.com>
2023-11-06 16:12:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  let data = req
+  console.log(data)
+  let ip = req.ip
+  let lang = req.headers['accept-language'];
+  let software = req.headers['user-agent']
+
+  res.json({
+    ipaddress: ip,
+    language: lang,
+    software: software
+  })
+})
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
